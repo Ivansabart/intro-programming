d22746f103afb24e8a1d0eaba77fa27d29595d23
Iván Salazar Bartolo <ivansabart@gmail.com>
2022-04-25 00:56:06 -0500

    add sección 3-4

---
+++ seccion-3/app.js
@@ -0,0 +1,7 @@
+// convierte de grados Fahrengeith a Centígrados
+let c;
+let f = 90;
+
+c = (f - 32)*(5 / 9);
+
+console.log(c);
---
+++ seccion-4/app.js
@@ -0,0 +1,13 @@
+let nombre = 'Ivan';
+let apellido = 'Salazar';
+
+let nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+let activo = true;
+let despedido = false;
